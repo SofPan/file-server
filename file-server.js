e36cafa7ff8bac2509cd34a6df456ec2816b2437
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-16 12:19:56 -0500

    handle connection event server-side

--- file-server.js
+++ file-server.js
@@ -5,3 +5,8 @@ const server = net.createServer();
 server.listen(5500, () => {
   console.log("Server listening on port 5500");
 });
+
+server.on("connection", client => {
+  console.log("New client connected");
+  client.write("Connection established");
+});
